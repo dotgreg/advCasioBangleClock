3c6b8d4815f832c7c8cccb592efa27d5705decc1
greg@1m <greg@1m>
2022-06-23 21:28:03 +0200

    up

--- web-app/server.js
+++
@@ -1,9 +0,0 @@
-const express = require('express')
-var fs = require('fs');
-var path = require('path');
-const app = express()
-const port = process.env.PORT || 5005;
-app.use('/', express.static('./client'))
-app.listen(port, () => {
-    console.log("Example app listening at http://localhost", port)
-})
